d9423922a81820ab54d9e8bf4c9dcd360ce0c831
nisreen-salih <fg.nisreen@gmail.com>
2024-01-03 12:01:38 -0800

    0x01

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def islower(l):
-    if ord(l) >= ord('a') and ord(l) <= ('z'):
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ('z'):
         return True
     else:
         return False
