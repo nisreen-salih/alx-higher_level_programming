460672025d1d96e867e02817eb95c54af48d756d
nisreen-salih <fg.nisreen@gmail.com>
2024-01-03 12:28:18 -0800

    0x01

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for a in range(1, 101):
         if a % 15 == 0:
-            print("FizzBuzz", end" ")
+            print("FizzBuzz", end=" ")
         elif a % 5 == 0:
             print("Buzz", end=" ")
         elif a % 3 == 0:
