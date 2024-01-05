e895e166ee80ae74d0115caa8f521e8a98c1c319
nisreen-salih <fg.nisreen@gmail.com>
2024-01-05 14:09:12 -0800

    0x02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
 
     count = len(sys.argv) - 1
     if count == 0:
-        print("0 arguments. ")
+        print("0 arguments.")
     elif count == 1:
-        print("1 argumwnts:")
+        print("1 argumwnt:")
     else:
         print("{} arguments:".format(count))
-        for i on range(count):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        for a in range(count):
+            print("{}: {}".format(a + 1, sys.argv[a + 1]))
