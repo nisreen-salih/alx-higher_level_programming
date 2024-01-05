99099405d44f7840d4aaa9cd2bae24762c35e5d1
nisreen-salih <fg.nisreen@gmail.com>
2024-01-05 14:21:47 -0800

    0x02

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ ==" __main__":
+    """print all hidden directories"""
+    import hidden_4
+
+    for a in dir(hidden_4):
+        if a[:2] != "__":
+            print(a)
