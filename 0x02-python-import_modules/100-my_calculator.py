4c9aa17c49b0eb2c4561550685769b95b9ec8072
nisreen-salih <fg.nisreen@gmail.com>
2024-01-05 14:41:31 -0800

    0x02

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(sys.arg[1])
+    a = int(sys.argv[1])
     b = int(sys.argv[3])
     op =sys.argv[2]
     if op == "+":
