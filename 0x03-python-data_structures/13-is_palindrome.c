20341a571afddb05b384a9ff90f4946ae594497c
nisreen-salih <fg.nisreen@gmail.com>
2024-01-08 12:09:34 -0800

    0x03

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include"lists.h"
+
+/**
+ *is_ palindrome - recursive
+ *
+ * @head: list head
+ *
+ * Return: 1 or 0
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (aux_palindrom(head, *hrad));
+}
+
+/**
+ * aux_palindrom - know palindrom
+ *
+ * @head: list head
+ * @end: list end
+ */
+
+int aux_palindrom(listint_t **head, listint_t *end)
+{
+	if (enf == NULL)
+		return (1);
+	if (aux_palindrom(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head->next;
+				return (1);
+	}
+	return (0);
+}
