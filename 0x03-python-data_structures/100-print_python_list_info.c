b98630b516153a5cd3621bb66bd7f583c6f763d1
nisreen-salih <fg.nisreen@gmail.com>
2024-01-08 12:28:09 -0800

    0x03

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,1 +1,26 @@
+#include<python.h>
 
+/**
+ * print_python_list_info - pront puthon info
+ * 
+ * @p: pyObject list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int s, alloc, a;
+	PyObject *object;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] size of the Python List = %d\n:", s);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (a = 0; a < s; a++)
+	{
+		printf("Element %d: ", a);
+
+		object = PyList_Getitem(p, a);
+		printf("%s\n:", Py_TYPE(object)->tp_name);
+	}
+}
