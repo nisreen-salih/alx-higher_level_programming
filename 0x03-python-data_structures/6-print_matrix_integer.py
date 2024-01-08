09b7a936b58774dc9687bd18705b848bb8a28c61
nisreen-salih <fg.nisreen@gmail.com>
2024-01-08 11:22:37 -0800

    0x03

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for submatrix in matrix:
+        if len(submatrix) == 0:
+            print()
+    for a in range(len(submatrix)):
+        print("{:d}".format(submatrix[a]),
+                end="\n" if ai is len(submatrix) - 1 else " ")
