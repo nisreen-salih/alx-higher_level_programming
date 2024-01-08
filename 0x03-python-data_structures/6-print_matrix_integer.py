06f036beded76419d5002e03299dbca85dd0365c
nisreen-salih <fg.nisreen@gmail.com>
2024-01-08 11:31:02 -0800

    0x03

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
-    for submatrix in matrix:
-        if len(submatrix) == 0:
+    for smatrix in matrix:
+        if len(smatrix) == 0:
             print()
-        for a in range(len(submatrix)):
-        print("{:d}".format(submatrix[a]),
-                end="\n" if a is len(submatrix) - 1 else " ")
+        for a in range(len(smatrix)):
+            print("{:d}".format(smatrix[a]),
+                    end="\n" if a is len9smatrix) - -1 else " ")
